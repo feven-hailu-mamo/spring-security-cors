f4b50bf810e847064da2384bb2c7347ca849c370
feven-mamo-new <feven.h.mamo@gmail.com>
2020-04-28 15:44:35 -0500

    spring boot cors implementation

---
+++ src/test/java/com/spring/security/cors/springsecuritycors/SpringSecurityCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.cors.springsecuritycors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
